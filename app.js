7ccd78b8527d8bffab84c252cda3ad246beaab57
ezaboroskie <zaboroskie.edward@gmail.com>
2022-10-18 18:14:48 -0400

    random drink now works for mobile and desktop and fixed visibility of divs

--- app.js
+++ app.js
@@ -446,51 +446,53 @@ async function getLetterZ() {
         allDrinks.innerHTML += drink.join("")
 }
 
-drinksAZ.addEventListener('click', function(){
-    
-    const azDrinks = async()=>{
-        try{
-            await getLetterA();
-            await getLetterB();
-            await getLetterC();
-            await getLetterD();
-            await getLetterE();
-            await getLetterF();
-            await getLetterG();
-            await getLetterH();
-            await getLetterI();
-            await getLetterJ();
-            await getLetterK();
-            await getLetterL();
-            await getLetterM();
-            await getLetterN();
-            await getLetterO();
-            await getLetterP();
-            await getLetterQ();
-            await getLetterR();
-            await getLetterS();
-            await getLetterT();
-            await getLetterV();
-            await getLetterW();
-            await getLetterY();
-            await getLetterZ();
+const azDrinks = async()=>{
+    try{
+        await getLetterA();
+        await getLetterB();
+        await getLetterC();
+        await getLetterD();
+        await getLetterE();
+        await getLetterF();
+        await getLetterG();
+        await getLetterH();
+        await getLetterI();
+        await getLetterJ();
+        await getLetterK();
+        await getLetterL();
+        await getLetterM();
+        await getLetterN();
+        await getLetterO();
+        await getLetterP();
+        await getLetterQ();
+        await getLetterR();
+        await getLetterS();
+        await getLetterT();
+        await getLetterV();
+        await getLetterW();
+        await getLetterY();
+        await getLetterZ();
 
-        } catch (err) {
-            console.error(err);
-        }
-
+    } catch (err) {
+        console.error(err);
     }
-    
+
+}
+
+drinksAZ.addEventListener('click', function(){
+
     azDrinks()
-       
+    
     
 })
 
 const randDrink = document.getElementById('randDrink')
 const largeImg = document.getElementById('largeImg')
 const drinkDescript = document.getElementById('drinkDescript')
+const contentAlign = document.getElementById('contentAlign')
+const rightContainer = document.getElementById('rightContainer')
 
-async function getRandomDrink() {
+async function getRandomDrinkMobile() {
     const url = "https://www.thecocktaildb.com/api/json/v2/9973533/random.php"
     let response = await fetch (url)
     let bevs = await response.json()
@@ -498,35 +500,78 @@ async function getRandomDrink() {
     let random = drinks.map(function(result) {
 
         return`
-        <h2><u>${result.strDrink}</u></h2>
-        <h3>Instructions:</h3>
+        <div class = "content-align" id="contentAlign">
+        <img class="large-img-mobile" src="${result.strDrinkThumb}">
+        <h2>${result.strDrink}</h2>
         <p>${result.strInstructions}</p>
-        <h3>Ingredients:</h3>
         <ul>
-            ${result.strIngredient1 ? `<li>${result.strIngredient1} - ${result.strMeasure1}</li>` : ""}
-            ${result.strIngredient2 ? `<li>${result.strIngredient2} - ${result.strMeasure2}</li>` : ""}
-            ${result.strIngredient3 ? `<li>${result.strIngredient3} - ${result.strMeasure3}</li>` : ""}
-            ${result.strIngredient4 ? `<li>${result.strIngredient4} - ${result.strMeasure4}</li>` : ""}
-            ${result.strIngredient5 ? `<li>${result.strIngredient5} - ${result.strMeasure5}</li>` : ""}
-            ${result.strIngredient6 ? `<li>${result.strIngredient6} - ${result.strMeasure6}</li>` : ""}
-            ${result.strIngredient7 ? `<li>${result.strIngredient7} - ${result.strMeasure7}</li>` : ""}
-            ${result.strIngredient8 ? `<li>${result.strIngredient8} - ${result.strMeasure8}</li>` : ""}
-            ${result.strIngredient9 ? `<li>${result.strIngredient9} - ${result.strMeasure9}</li>` : ""}
-            ${result.strIngredient10 ? `<li>${result.strIngredient10} - ${result.strMeasure10}</li>` : ""}
-            ${result.strIngredient11 ? `<li>${result.strIngredient11} - ${result.strMeasure11}</li>` : ""}
-            ${result.strIngredient12 ? `<li>${result.strIngredient12} - ${result.strMeasure12}</li>` : ""}
-            ${result.strIngredient13 ? `<li>${result.strIngredient13} - ${result.strMeasure13}</li>` : ""}
-            ${result.strIngredient14 ? `<li>${result.strIngredient14} - ${result.strMeasure14}</li>` : ""}
-            ${result.strIngredient15 ? `<li>${result.strIngredient15} - ${result.strMeasure15}</li>` : ""}
-        </ul>
-        <img src="${result.strDrinkThumb}" />
+                ${result.strIngredient1 ? `<li>${result.strIngredient1} - ${result.strMeasure1}</li>` : ""}
+                ${result.strIngredient2 ? `<li>${result.strIngredient2} - ${result.strMeasure2}</li>` : ""}
+                ${result.strIngredient3 ? `<li>${result.strIngredient3} - ${result.strMeasure3}</li>` : ""}
+                ${result.strIngredient4 ? `<li>${result.strIngredient4} - ${result.strMeasure4}</li>` : ""}
+                ${result.strIngredient5 ? `<li>${result.strIngredient5} - ${result.strMeasure5}</li>` : ""}
+                ${result.strIngredient6 ? `<li>${result.strIngredient6} - ${result.strMeasure6}</li>` : ""}
+                ${result.strIngredient7 ? `<li>${result.strIngredient7} - ${result.strMeasure7}</li>` : ""}
+                ${result.strIngredient8 ? `<li>${result.strIngredient8} - ${result.strMeasure8}</li>` : ""}
+                ${result.strIngredient9 ? `<li>${result.strIngredient9} - ${result.strMeasure9}</li>` : ""}
+                ${result.strIngredient10 ? `<li>${result.strIngredient10} - ${result.strMeasure10}</li>` : ""}
+                ${result.strIngredient11 ? `<li>${result.strIngredient11} - ${result.strMeasure11}</li>` : ""}
+                ${result.strIngredient12 ? `<li>${result.strIngredient12} - ${result.strMeasure12}</li>` : ""}
+                ${result.strIngredient13 ? `<li>${result.strIngredient13} - ${result.strMeasure13}</li>` : ""}
+                ${result.strIngredient14 ? `<li>${result.strIngredient14} - ${result.strMeasure14}</li>` : ""}
+                ${result.strIngredient15 ? `<li>${result.strIngredient15} - ${result.strMeasure15}</li>` : ""}
+            </ul>
+        </div>
         `
     })
  
+    allDrinks.innerHTML = random.join("")
 }
 
+async function getRandomDrinkDesktop() {
+    const url = "https://www.thecocktaildb.com/api/json/v2/9973533/random.php"
+    let response = await fetch (url)
+    let bevs = await response.json()
+    let drinks = bevs.drinks
+    let random = drinks.map(function(result) {
+
+        return`
+        <div class = "bottom-right-container" id="rightContainer">
+        <img class="large-img-mobile" src="${result.strDrinkThumb}">
+        <h2>${result.strDrink}</h2>
+        <p>${result.strInstructions}</p>
+        <ul>
+                ${result.strIngredient1 ? `<li>${result.strIngredient1} - ${result.strMeasure1}</li>` : ""}
+                ${result.strIngredient2 ? `<li>${result.strIngredient2} - ${result.strMeasure2}</li>` : ""}
+                ${result.strIngredient3 ? `<li>${result.strIngredient3} - ${result.strMeasure3}</li>` : ""}
+                ${result.strIngredient4 ? `<li>${result.strIngredient4} - ${result.strMeasure4}</li>` : ""}
+                ${result.strIngredient5 ? `<li>${result.strIngredient5} - ${result.strMeasure5}</li>` : ""}
+                ${result.strIngredient6 ? `<li>${result.strIngredient6} - ${result.strMeasure6}</li>` : ""}
+                ${result.strIngredient7 ? `<li>${result.strIngredient7} - ${result.strMeasure7}</li>` : ""}
+                ${result.strIngredient8 ? `<li>${result.strIngredient8} - ${result.strMeasure8}</li>` : ""}
+                ${result.strIngredient9 ? `<li>${result.strIngredient9} - ${result.strMeasure9}</li>` : ""}
+                ${result.strIngredient10 ? `<li>${result.strIngredient10} - ${result.strMeasure10}</li>` : ""}
+                ${result.strIngredient11 ? `<li>${result.strIngredient11} - ${result.strMeasure11}</li>` : ""}
+                ${result.strIngredient12 ? `<li>${result.strIngredient12} - ${result.strMeasure12}</li>` : ""}
+                ${result.strIngredient13 ? `<li>${result.strIngredient13} - ${result.strMeasure13}</li>` : ""}
+                ${result.strIngredient14 ? `<li>${result.strIngredient14} - ${result.strMeasure14}</li>` : ""}
+                ${result.strIngredient15 ? `<li>${result.strIngredient15} - ${result.strMeasure15}</li>` : ""}
+            </ul>
+        </div>
+        `
+    })
+ 
+    rightContainer.innerHTML = random.join("")
+}
+
+
 randDrink.addEventListener('click', function(){
-    getRandomDrink()
+    if (screenSize.matches){
+        getRandomDrinkDesktop()
+    }else{
+        getRandomDrinkMobile()
+    }
 })
 
 var screenSize = window.matchMedia("(min-width: 750px)")
+
